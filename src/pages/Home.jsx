0b0a1c003b8fde9cb9824c255422dbf59679e754
amiraydin <amiraydin@outlook.fr>
2021-11-05 13:53:38 +0100

    ready to pull 2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import avion from '../images/airplan.jpg'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import image from '../images/deuxiem.jpg'
+
 
 export default function Home() {
     return (
-        <div>
+        <>
+            <div className="container" >
+                <div>
+                    <img src={avion} alt="plan" style={{ width: "190vh", height: "50vw" }} />
+                </div>
+                <div className="d-flex p-3">
+                    <div className="text-center">
+                        <h2>fly fly</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus nulla repellendus aut est tempora laborum provident facilis vero? Cupiditate facere doloribus commodi sunt repudiandae quia quos veniam dolores ab earum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam officiis molestiae, culpa ab natus quia nisi. Porro, voluptatum, repudiandae hic autem tenetur quis iure quia nobis quasi temporibus consequatur ipsam.</p>
+                    </div>
+                    <div className="text-center">
+                        <h2>fly fly</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur numquam perferendis modi alias minus explicabo, eveniet aspernatur blanditiis similique at. Doloribus debitis perferendis rerum delectus explicabo esse, iure at accusamus? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur qui necessitatibus fugiat explicabo totam minus, veniam id ipsam amet dicta deleniti fuga voluptate commodi repudiandae aut libero, quisquam a perspiciatis?</p>
+                    </div>
+                </div>
+                <a href="/Avions">
+                    <img src={image} alt="deuxiem" style={{ width: "190vh", height: "50vw", backgroundColor: "(0,0,0,0.50)" }} />
+                </a>
 
-        </div>
+            </div>
+        </>
     )
 }
+
