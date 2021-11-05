f7738935d4d74f743d38ae8a8d4d3312b5362ce2
ishranm9@gmail.com <ishranm9@gmail.com>
2021-11-05 12:38:45 +0100

    ready to pull

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./pages/Home.jsx";
@@ -8,16 +7,10 @@ import Reserch from "./pages/Reserch";
 import Navbar from "./component/Navbar.jsx";
 import Login from "./pages/Login";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-=======
-// import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
->>>>>>> 0a22b5ef9b72259210d2daa513142de7dc103a1b
 
 export default function App() {
   return (
     <div>
-<<<<<<< HEAD
       <BrowserRouter>
         <Navbar />
         <Routes>
@@ -28,9 +21,6 @@ export default function App() {
           <Route path="/Login" element={<Login />} />
         </Routes>
       </BrowserRouter>
-=======
-      <h1>hello</h1>
->>>>>>> 0a22b5ef9b72259210d2daa513142de7dc103a1b
     </div>
   );
 }
