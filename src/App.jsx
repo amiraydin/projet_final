7504e31d1cc28186326fa0d002b12399a7795d11
Mohamed-Amir kharmaz <amirpro@hotmail.fr>
2021-11-08 10:06:36 +0100

    first commit Plane

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import './App.css';
+import Avion from './component/Avion';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import styled from "styled-components"
+
 
 export default function App() {
+  const AppContainer = styled.div`
+  width : 100%;
+  height : 100%;
+  display : flex;
+  align-items:center;
+  justify-content : center;
+  background-color : grey;
+  `;
+
   return (
     <div className="App">
-      <h1>hello</h1>
+      <AppContainer> Jet Sky </AppContainer>
+      <Avion />
     </div>
   );
 }
-
