2b95b4e48d6e268ac0c85ce55712ef9360fce40d
sofianeSghiouar <sofianesgh@gmail.com>
2021-11-14 22:06:10 +0100

    testing some style on aircraft page

--- src/pages/Avions.jsx
+++ src/pages/Avions.jsx
@@ -1,15 +1,20 @@
 import React, { useEffect, useState } from "react";
-
+import Typical from 'react-typical'
 import CardPlane from "../component/Card.avion";
-
+// import neon from '../video/neonRedBlue.mp4'
 
 export default function Avions() {
-  const [images, setImages] = useState([])
+  const [categories, setCategories] = useState('')
+
+  // useEffect(() => {
+  //   fetch('https://picsum.photos/v2/list?page=2&limit=10')
+  //     .then(res => res.json())
+  //     .then(json => setImages(json))
+  // }, [])
+
 
   useEffect(() => {
-    fetch('https://picsum.photos/v2/list?page=2&limit=10')
-      .then(res => res.json())
-      .then(json => setImages(json))
+    setCategories('')
   }, [])
 
 
@@ -18,15 +23,33 @@ export default function Avions() {
   return (
 
     <>
-      <div className="header">
-      </div>
-      <div className='container-card-avion'>
+      {categories ?
+        <div className='containerJetCategory'>
+          <h1 style={{ position: "absolute", top: "20%", left: "17%", color: "white", zIndex: "0", width: "auto" }}>
+            <Typical loop={1} wrapper='b' steps={[
+              'Model', 1000, categories, 1000
+            ]} />
+          </h1>
 
-        {images && images.map(image => (
-          <CardPlane images={image} />
-        ))}
-      </div>
 
+
+        </div>
+
+        :
+        <>
+          <div className="header">
+            <h1 style={{ position: "absolute", top: "20%", left: "17%", color: "white", zIndex: "0", width: "auto" }}><strong>Check</strong>  {' '}
+              <Typical loop={1} wrapper='b' steps={[
+                'Our Light Jets', 1000, 'Our Medium Jets', 1000, 'Our Heavy Jets', 1000, 'that shit !', 100
+              ]} />
+            </h1>
+          </div>
+          <div className='container-card-avion'>
+          
+            <CardPlane categories={setCategories} />
+          </div>
+        </>
+      }
     </>
 
   );
