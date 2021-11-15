8df5fe60c86f30ea85b495ed4bf75b3e3bc9474b
sofianeSghiouar <sofianesgh@gmail.com>
2021-11-15 20:39:02 +0100

    uptade some style

--- src/component/Data.Jets.jsx
+++ src/component/Data.Jets.jsx
@@ -21,12 +21,12 @@ const DataJets = (props) => {
         <div className='container-detail-avion'>
 
 
-            <h1 style={{ position: "absolute", top: "20%", left: "5%", color: "white", zIndex: "0", width: "auto" }}>
+            <h1 style={{ position: "absolute", top: "20%", left: "5%", color: "white", zIndex: "222", width: "auto" }}>
                 <Typical loop={1} wrapper='b' steps={[
                     'Model', 1000, chosenCategory.title, 1000
                 ]} />
             </h1>
-            <button onClick={() => window.location.reload()} >return</button>
+            <button className="btn-return-aircrafts" onClick={() => window.location.reload()} >retour</button>
 
             {chosenCategory &&
                 <>
@@ -34,29 +34,37 @@ const DataJets = (props) => {
                         <img className="img-header" src={chosenCategory.images.exterior} alt={chosenCategory.title} />
                     </div>
                     <section className="description-specification">
-                        <div className="dataInfo">
-                            <h2>{chosenCategory.title}</h2>
+                        <div className="aircraft-info">
+                            <div className="title-lines">
+                                <h2>{chosenCategory.title}</h2>
+                                <div className="lines">
+                                    <div className="line-1"></div>
+                                    <div className="line-2"></div>
+                                    <div className="line-3"></div>
+                                </div>
+                            </div>
                             <p>{chosenCategory.description}</p>
                         </div>
-                        <div className="specification">
+                        <div className="aircraft-specification">
+
                             <ul className="list-detail-ul">
-                                <li className="listData">PASSENGER CAPACITY:<span>{chosenCategory.specification['PASSENGER CAPACITY']}</span></li>
+                                <li className="list-detail-li">PASSENGER CAPACITY:<span>{chosenCategory.specification['PASSENGER CAPACITY']}</span></li>
 
-                                <li className="listData">MAX RANGE:<span>{chosenCategory.specification['MAX RANGE']}</span></li>
+                                <li className="list-detail-li">MAX RANGE:<span>{chosenCategory.specification['MAX RANGE'][1]}</span></li>
 
-                                <li className="listData">MAX OPERATIONAL SPEED:<span>{chosenCategory.specification['MAX OPERATIONAL SPEED']}</span></li>
+                                <li className="list-detail-li">MAX OPERATIONAL SPEED:<span>{chosenCategory.specification['MAX OPERATIONAL SPEED'][1]}</span></li>
 
-                                <li className="listData">CABIN HEIGHT:<span>{chosenCategory.specification['CABIN HEIGHT']}</span></li>
+                                <li className="list-detail-li">CABIN HEIGHT:<span>{chosenCategory.specification['CABIN HEIGHT'][1]}</span></li>
 
-                                <li className="listData">CABIN WIDTH:<span>{chosenCategory.specification['CABIN WIDTH']}</span></li>
+                                <li className="list-detail-li">CABIN WIDTH:<span>{chosenCategory.specification['CABIN WIDTH'][1]}</span></li>
 
-                                <li className="listData">CABIN LENGTH:<span>{chosenCategory.specification['CABIN LENGTH']}</span></li>
+                                <li className="list-detail-li">CABIN LENGTH:<span>{chosenCategory.specification['CABIN LENGTH'][1]}</span></li>
 
                             </ul>
                         </div>
                     </section>
 
-                    <section className="img-cabin">
+                    <section >
                         <img className="img-cabin" src={chosenCategory.images.cabin} alt={chosenCategory.title} />
                     </section>
                 </>
